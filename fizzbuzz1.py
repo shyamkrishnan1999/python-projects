e665235cbb970ff7e0949692b4ba3b74e05a0432
shyam <shyamkrishnan1999@gmail.com>
2020-07-11 21:36:42 +0530

    Some minor changes

--- fizzbuzz1.py
+++ fizzbuzz1.py
@@ -1,9 +1,8 @@
-count,num=0,0
-while count<=100:
+num=0
+while num<=100:
     num+=1
     if num%3==0 and num%5==0:
         print("FizzBuzz",end=" ")
-        count+=1
     elif num%5==0:
         print("Buzz",end=" ")
     elif num%3==0:
--- fizzbuzzplop.py
+++ fizzbuzzplop.py
@@ -1,27 +1,23 @@
 cond=int(input())
 cond_values=list(map(int,input().split(" ")))
 cond_words=list(input().split(" "))
-count=0
 num=0
-Str,string="",""
-
-for i in cond_words:
-    string=string+i
+string=""
 
 
 
-while count<=100:
+
+while num<=100:
     num+=1
+    word_str=""
     for i in range(0,len(cond_values)):
         if num%cond_values[i]==0:
-            Str=Str+cond_words[i]
-    if str=="":
+            word_str=word_str+cond_words[i]
+    if word_str=="":
         print(num,end=" ")
     else:
-        print(Str,end=" ")
+        print(word_str,end=" ")
 
-    if Str==string:
-        count+=1
 
 
 

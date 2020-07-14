d41655164cebff47e194a6c60826999a6506a914
shyam <shyamkrishnan1999@gmail.com>
2020-07-14 10:19:40 +0530

    small change

--- substring/substring.py
+++ substring/substring.py
@@ -2,8 +2,15 @@ word=input()
 
 charset=set()
 mod_word=word.lower()
+length=i=j=0
 
-for i in range(0,len(mod_word)):
-    charset.add(mod_word[i])
+while j<len(mod_word):
+    if mod_word[j] in charset:
+        charset.remove(mod_word[i])
+        i+=1
+    else:
+        charset.add(mod_word[j])
+        j+=1
+        length=max(len(mod_word),length)
 
 input(len(charset))

bd265831d4b062f5232562f96fa03d4d88072bd5
shyam <shyamkrishnan1999@gmail.com>
2021-03-25 12:32:40 +0530

    Practicing python

---
+++ practice python/string.py
@@ -0,0 +1,22 @@
+
+
+#List of fruits
+fruits=['Apple','Orange','Banana','Lemon','Papaya']
+
+
+for fruit in fruits:
+    #Using concatenation operator
+    print('I love '+fruit)
+
+    #using join operator
+    print(' '.join(['I love',fruit]))
+
+    #using format operator
+    print('I love {}'.format(fruit))
+
+    print("\n")
+
+
+
+
+
